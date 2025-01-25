1bf70352b786ffc1f94459b53db87983fcd989e9
Vizzent <vizzent@gmail.com>
2025-01-25 07:42:20 +0100

    Simplify Menu UI

--- Scenes/Menu/menu.gd
+++ Scenes/Menu/menu.gd
@@ -1,12 +1,19 @@
-extends Node2D
+extends Control
 
 @onready var play_scene: PackedScene = preload("res://Scenes/SwordGame/Move/game.tscn")
 @onready var play_keep_scene: PackedScene = preload("res://Scenes/SwordGame/Keep/game.tscn")
-@onready var options_panel : Panel = $CanvasLayer/Control/OptionsPanel
-@onready var credits_panel : Panel = $CanvasLayer/Control/CreditsPanel
+@onready var options_panel : Panel = $OptionsPanel
+@onready var credits_panel : Panel = $CreditsPanel
 
 func _ready() -> void:
 	MusicController.switch_to_menu_song()
+	$VBoxContainer/PlayButton.pressed.connect(_on_play_button_pressed)
+	$VBoxContainer/PlayButton2.pressed.connect(_on_play_keep_button_pressed)
+	$VBoxContainer/Options.pressed.connect(_on_options_pressed)
+	$VBoxContainer/Credits.pressed.connect(_on_credits_pressed)
+	$VBoxContainer/PlayButton.pressed.connect(_on_play_button_pressed)
+	$CreditsPanel/CloseButton.pressed.connect(_on_close_button_pressed)
+	$OptionsPanel/CloseButton.pressed.connect(_on_close_button_pressed)
 
 func _on_play_button_pressed() -> void:
 	SfxAudioPlayer.play("MouseClick")
