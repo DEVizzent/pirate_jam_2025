b97515311809d6ec418d7d2a279046cce7067621
DEVizzent <vizzent@gmail.com>
2025-01-31 00:09:19 +0100

    Add option to end the game from the dialog system

--- Autoloads/DialogueEventBus/dialogue_event_bus.gd
+++ Autoloads/DialogueEventBus/dialogue_event_bus.gd
@@ -1,6 +1,14 @@
 extends Node
 
 signal play_character_animation(character: String, animation: String)
+signal the_end()
+signal game_over()
 
 func animate(character: String, animation: String) -> void:
 	play_character_animation.emit(character, animation)
+
+func throw_the_end() -> void:
+	the_end.emit()
+	
+func throw_game_over() -> void:
+	game_over.emit()
