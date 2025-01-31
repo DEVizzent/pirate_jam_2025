cc3a917d86e2c5a233f0198edc6953ddf7482d78
DEVizzent <vizzent@gmail.com>
2025-01-31 13:23:00 +0100

    Update stats with new icons

--- Scenes/UI/Stats/ui_stats.gd
+++ Scenes/UI/Stats/ui_stats.gd
@@ -6,6 +6,7 @@ const UPDATE_ICON_TIME : float = 2.0
 @onready var food_icon : TextureRect = $Panel/MarginContainer/HBoxContainer/FoodIcon
 @onready var force_icon : TextureRect = $Panel/MarginContainer/HBoxContainer/ForceIcon
 
+var pagan_icon: Texture2D = load("res://Character Models/textures/ICONS/runa64.png")
 
 func _ready() -> void:
 	update_faith(0, KingdomStats.faith)
@@ -14,6 +15,7 @@ func _ready() -> void:
 	EventBus.faith_changed.connect(update_faith)
 	EventBus.food_changed.connect(update_food)
 	EventBus.force_changed.connect(update_force)
+	EventBus.pagan_state.connect(change_to_pagan)
 
 func update_faith(_from: int, to : int) -> void:
 	tween_update(faith_icon, to)
@@ -21,6 +23,8 @@ func update_food(_from: int, to : int) -> void:
 	tween_update(food_icon, to)
 func update_force(_from: int, to : int) -> void:
 	tween_update(force_icon, to)
+func change_to_pagan() -> void:
+	faith_icon.texture = pagan_icon
 
 func tween_update(icon : TextureRect, to: int) -> void:
 	var tween : Tween = get_tree().create_tween()
